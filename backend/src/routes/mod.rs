6fb16e78a402f5223cffbb489581bdfca3679184
Tricked <72335827+Tricked-dev@users.noreply.github.com>
2023-06-30 16:09:39 +0200

    feat: port to axum (#6)
    * feat: port to axum
    
    * apply fixes
    
    * fix sparse & rustfmt config

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,4 +1,5 @@
-use actix_web::{error, HttpResponse};
+use axum::{body::Bytes, http::Response, response::IntoResponse, Json, Router};
+use http_body::combinators::UnsyncBoxBody;
 use reqwest::StatusCode;
 use thiserror::Error;
 
@@ -6,6 +7,9 @@ use crate::models;
 
 pub mod player;
 pub mod profile;
+
+pub type SkyResult<T> = Result<T, ApiError>;
+pub type RouterResponse = Router<(), axum::body::Body>;
 
 #[derive(Error, Debug)]
 pub enum ApiError {
@@ -17,23 +21,31 @@ pub enum ApiError {
     ProfileNotFound { username: String, profile: String },
 }
 
-impl error::ResponseError for ApiError {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(self.status_code()).json(models::error::ApiError {
-            error: match self {
-                ApiError::Mojang(_) => "mojang",
-                ApiError::Hypixel(_) => "hypixel",
-                ApiError::ProfileNotFound { .. } => "profile_not_found",
-            },
-            description: &self.to_string(),
-        })
-    }
-
+impl ApiError {
     fn status_code(&self) -> StatusCode {
-        match *self {
-            ApiError::Mojang(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            ApiError::Hypixel(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        match self {
+            ApiError::Mojang(_) | ApiError::Hypixel(_) => StatusCode::INTERNAL_SERVER_ERROR,
             ApiError::ProfileNotFound { .. } => StatusCode::NOT_FOUND,
         }
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response<UnsyncBoxBody<Bytes, axum::Error>> {
+        let status_code = self.status_code();
+
+        let error = match self {
+            ApiError::Mojang(_) => "mojang",
+            ApiError::Hypixel(_) => "hypixel",
+            ApiError::ProfileNotFound { .. } => "profile_not_found",
+        };
+
+        let description = self.to_string();
+
+        let api_error = models::error::ApiError {
+            error,
+            description: &description,
+        };
+        (status_code, Json(api_error)).into_response()
     }
 }
