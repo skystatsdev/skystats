fa4805881c10e9a3b04731efef36a78e1bc0c6ca
mat <27899617+mat-1@users.noreply.github.com>
2023-07-01 20:07:36 -0500

    Show Hypixel errors in backend response (#16)
    Co-authored-by: mat <git@matdoes.dev>

--- backend/src/models/hypixel/mod.rs
+++ backend/src/models/hypixel/mod.rs
@@ -1,5 +1,22 @@
 //! Serde models for the Hypixel API.
 
+use serde::Deserialize;
+use thiserror::Error;
+
 pub mod inventory;
 pub mod player;
 pub mod profiles;
+
+#[derive(Deserialize, Error, Debug)]
+#[error("{cause}")]
+pub struct HypixelApiError {
+    pub success: bool,
+    pub cause: String,
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum HypixelApiResult<T> {
+    Success(T),
+    Error(HypixelApiError),
+}
