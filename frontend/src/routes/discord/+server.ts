5846feb0c8c3a63913a263bb386f1322088a5dcd
Kaeso <24925519+ptlthg@users.noreply.github.com>
2023-07-11 13:55:00 -0400

    Update frontend/src/routes/discord/+server.ts

--- frontend/src/routes/discord/+server.ts
+++ frontend/src/routes/discord/+server.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import { PUBLIC_DISCORD_INVITE } from '$env/static/public';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({}) => {
+export const GET: RequestHandler = () => {
 	throw redirect(308, PUBLIC_DISCORD_INVITE);
 };
