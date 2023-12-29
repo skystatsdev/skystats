a5e335cc7a1ab50ef0212530727d3f6c56957df6
DuckySoLucky <robertkovac160@gmail.com>
2023-12-29 22:48:42 +0100

    fix: oops unused plugin

--- src/routes/@[player=playerId]/+layout.server.ts
+++ src/routes/@[player=playerId]/+layout.server.ts
@@ -1,4 +1,4 @@
-import { FetchMinecraftAccount } from '$api/mojang';
+import { fetchMinecraftAccount } from '$api/mojang';
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 import { getPlayer, getProfile } from '$api/hypixel';
@@ -6,7 +6,7 @@ import { getPlayer, getProfile } from '$api/hypixel';
 export const load = (async ({ params }) => {
 	const { player } = params;
 
-	const account = await FetchMinecraftAccount(player);
+	const account = await fetchMinecraftAccount(player);
 
 	if (!account) {
 		throw error(404, 'Player not found!');
