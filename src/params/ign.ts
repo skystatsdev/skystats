1da56b1eee3f1866066af077552431cf7938705d
Ty Chermsirivatana <professornitro@gmail.com>
2023-12-30 18:24:28 -0500

    Update ign.ts

--- src/params/ign.ts
+++ src/params/ign.ts
@@ -1,7 +1,6 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-
-export const isUsername: ParamMatcher = (param) => {
-	//This Regex should be able to handle legacy IGNs
-	return param.length <= 24 && /^[a-zA-Z0-9_]+$/.test(param);
-};
+export const isUsername = ((param: string): boolean => {
+    //This should handle Legacy IGNs
+    return param.length <= 24 && /^[a-zA-Z0-9_]+$/.test(param);
+}) satisfies ParamMatcher;
