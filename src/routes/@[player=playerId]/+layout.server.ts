469eab413ab7868eb1b0e8ca752401494027b2ca
DuckySoLucky <robertkovac160@gmail.com>
2023-12-30 22:18:10 +0100

    fix: svelte error

--- src/routes/@[player=playerId]/+layout.server.ts
+++ src/routes/@[player=playerId]/+layout.server.ts
@@ -1,7 +1,7 @@
 import { fetchMinecraftAccount } from '$api/mojang';
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-import { getPlayer, getProfile } from '$api/hypixel';
+import { getPlayer, getProfiles } from '$api/hypixel';
 
 export const load = (async ({ params }) => {
 	const { player } = params;
@@ -18,7 +18,7 @@ export const load = (async ({ params }) => {
 		throw error(404, 'Player data not found!');
 	}
 
-	const profiles = await getProfile(account.id);
+	const profiles = await getProfiles(account.id);
 
 	if (!profiles || profiles.success === false || profiles.profiles.length === 0) {
 		throw error(404, 'Profiles not found!');
