a8a48bd4415bbb76167167e830fa7e7044f00083
Ty Chermsirivatana <professornitro@gmail.com>
2023-12-30 18:20:59 -0500

    Update ign.ts

--- src/params/ign.ts
+++ src/params/ign.ts
@@ -1,5 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
+
 export const isUsername: ParamMatcher = (param) => {
+	//This Regex should be able to handle legacy IGNs
 	return param.length <= 24 && /^[a-zA-Z0-9_]+$/.test(param);
 };
