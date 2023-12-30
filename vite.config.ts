682143ce509ec4f0c1ba7f89eb444eaf6c589de2
Ty Chermsirivatana <professornitro@gmail.com>
2023-12-30 18:34:16 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			enabled: true,
+		},
+		watch: true,
+		setupFiles: ['./test/setup.ts'],
+		testTimeout: 5000,
+		isolate: false,
+		reporters: 'default',
+		silent: false,
+	}
 });
