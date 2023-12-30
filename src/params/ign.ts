2cd29fc15d8096408ae1c03fdf4f9d93cd5e4039
Ty Chermsirivatana <professornitro@gmail.com>
2023-12-30 23:46:02 +0000

    Fixed Match Function Requirement

--- src/params/ign.ts
+++ src/params/ign.ts
@@ -1,6 +1,6 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-export const isUsername = ((param: string): boolean => {
-    //This should handle Legacy IGNs
+export const match: ParamMatcher = (param: string): boolean => {
+    // This should handle Legacy IGNs
     return param.length <= 24 && /^[a-zA-Z0-9_]+$/.test(param);
-}) satisfies ParamMatcher;
+};
--- src/params/uuid.ts
+++ src/params/uuid.ts
@@ -1,6 +1,6 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-export const isUUID: ParamMatcher = (param) => {
+export const match: ParamMatcher = (param: string): boolean => {
     const uuidRegex = /^[a-fA-F0-9]+$/;
     const uuidWithDashesRegex = /^[a-fA-F0-9-]+$/;
 
