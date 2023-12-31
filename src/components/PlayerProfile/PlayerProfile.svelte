86b46119e176365eae02fa89c74d0d6363104229
tonydawhale <tonydawhale@gmail.com>
2023-12-31 14:18:43 -0800

    Revert to Kaeso rank color stuff

--- src/components/PlayerProfile/PlayerProfile.svelte
+++ src/components/PlayerProfile/PlayerProfile.svelte
@@ -21,9 +21,6 @@
 	$: profiles = playerProfiles
 		.filter((profile) => profile.id !== selectedProfile?.id)
 		.sort((a, b) => b.cuteName.localeCompare(a.cuteName));
-
-	$: rankColor = Object.values(RANKS).find((rank) => rank.color === rankData?.color)?.color;
-	$: rankPlus = Object.values(RANK_PLUS_COLORS).find((color) => color === rankData?.plusColor);
 </script>
 
 <div class="text-[36px] mt-[50px] mb-[20px] flex flex-wrap gap-x-[10px] gap-y-[8px] items-center">
@@ -34,12 +31,12 @@
 				<div
 					class="inline-flex text-[18px] h-[34px] leading-[34px] mt-[10px] ml-[-5px] rounded-[100px] align-top font-[700] overflow-clip"
 				>
-					<div class="bg-[{rankColor}] px-[15px] inline-block">
+					<div class="bg-[{rankData.color}] px-[15px] inline-block">
 						{rankData.tag}
 					</div>
 					{#if rankData.plus}
 						<div
-							class="bg-[{rankPlus}] pr-[8px] relative z-[1] inline-block before:content-[''] before:z-[-1] before:absolute before:top-0 before:bottom-[-0.1rem] before:left-[-7px] before:right-0 before:[transform:skew(-20deg)] before:bg-inherit"
+							class="bg-[{rankData.plusColor}] pr-[8px] relative z-[1] inline-block before:content-[''] before:z-[-1] before:absolute before:top-0 before:bottom-[-0.1rem] before:left-[-7px] before:right-0 before:[transform:skew(-20deg)] before:bg-inherit"
 						>
 							{rankData.plus}
 						</div>
