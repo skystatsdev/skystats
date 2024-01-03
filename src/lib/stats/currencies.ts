c731ce66f82b979af27b90d7072b3dcf28f4c25a
tonydawhale <tonydawhale@gmail.com>
2024-01-02 19:55:36 -0800

    Fix Types

--- src/lib/stats/currencies.ts
+++ src/lib/stats/currencies.ts
@@ -1,16 +1,16 @@
 import { ESSENCE } from '$constants';
-import type { SkyblockEssence, SkyblockProfile, SkyblockProfileMember } from '$types/hypixel';
+import type { Essence, SkyBlockProfile, SkyBlockProfileMember } from '$types/hypixel';
 
-export function getCurrencies(userProfile: Partial<SkyblockProfileMember>, profile: Partial<SkyblockProfile>) {
+export function getCurrencies(userProfile: Partial<SkyBlockProfileMember>, profile: Partial<SkyBlockProfile>) {
 	return {
 		bank: profile.banking?.balance ?? 0,
 		purse: userProfile.currencies?.coin_purse ?? 0,
 		motes: userProfile.currencies?.motes_purse ?? 0,
 		essence: Object.fromEntries(
 			Object.entries(ESSENCE).map(([essence]) => {
-				if (!userProfile.currencies?.essence?.[essence.toUpperCase() as SkyblockEssence]) return [essence, 0];
-				return [essence, userProfile.currencies?.essence[essence.toUpperCase() as SkyblockEssence].current ?? 0];
+				if (!userProfile.currencies?.essence?.[essence.toUpperCase() as Essence]) return [essence, 0];
+				return [essence, userProfile.currencies?.essence[essence.toUpperCase() as Essence].current ?? 0];
 			})
-		) as Record<Lowercase<SkyblockEssence>, number>
+		) as Record<Lowercase<Essence>, number>
 	};
 }
