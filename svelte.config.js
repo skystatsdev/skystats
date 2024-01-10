3f3fa66d2e477268354c735a3aade8eae4bfb188
DuckySoLucky <robertkovac160@gmail.com>
2024-01-10 22:07:14 +0100

    fix: dont touch svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
